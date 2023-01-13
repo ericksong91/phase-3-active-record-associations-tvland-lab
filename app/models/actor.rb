d7766c2ec11cd9730ecd33145c75633a2afcdac9
Eric Song <ericksong91@gmail.com>
2023-01-13 16:43:01 -0500

    Done with assignment

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        self.shows.map do |show|
+            "#{show.characters[0].name} - #{show.name}"
+        end
+    end
   
 end
